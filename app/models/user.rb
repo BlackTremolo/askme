9e792a3664a983fb32dfe66565cbe1a6aad23167
BlackTremolo <mihsee436l@gmail.com>
2026-04-08 17:15:59 +0800

    Add reference questions to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :questions
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_08_073216) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_08_085928) do
   create_table "questions", force: :cascade do |t|
     t.string "answer"
     t.datetime "created_at", null: false
     t.string "text"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,4 +30,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_08_073216) do
     t.datetime "updated_at", null: false
     t.string "username"
   end
+
+  add_foreign_key "questions", "users"
 end
