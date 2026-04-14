be8407994d7250a96a9d709a90342bf1793ba9d8
BlackTremolo <mihsee436l@gmail.com>
2026-04-14 13:12:14 +0800

    Add user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь зарегестрирован'
+    end
+  end
   def edit
   end
 
@@ -16,5 +24,12 @@ class UsersController < ApplicationController
       Question.new(text: 'Как дела?', created_at: Date.parse('13.04.2026'))
     ]
     @question_new = Question.new
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :email, :username, :password, :password_confirmation, :avatar_url)
   end
 end
